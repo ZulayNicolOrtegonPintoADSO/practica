00ddcf6c50557e651db960ba913e2ebfe6e79d44
ZulayNicolOrtegónPintoADSO <zulyortegon15@gmail.com>
2023-03-26 23:27:03 -0500

    feat: :art: v-show

---
+++ app.js
@@ -0,0 +1,5 @@
+
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
+import Mycomponent from "./javascript/archivo.js"
+
+createApp(Mycomponent).mount("#app")
---
+++ javascript/archivo.js
@@ -0,0 +1,13 @@
+export default{
+    data(){
+        return {
+            op1: false,
+            inputeliminar: null,
+
+            op2: false,
+            inputagregar: null
+            
+        
+        }
+    }
+}
